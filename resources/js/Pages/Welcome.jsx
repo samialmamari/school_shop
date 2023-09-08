3c3b3fbfcea67410d62f46e7006a0171fc78d582
sami <sami.almamai@css.edu.om>
2023-09-08 18:06:37 +0400

    welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,49 +1,21 @@
-import { Link, Head , useForm} from '@inertiajs/react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
 
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-
-import TextInput from '@/Components/TextInput';
-import { useState,  useEffect, useRef  } from 'react';
-
-
-export default function Welcome() {
-    const [nfcSupported, setNfcSupported] = useState(null);
-
-    useEffect(() => {
-      // Check if Web NFC API is supported
-      if ('NDEFReader' in window) {
-        setNfcSupported(true);
-        startNfcScanning(); // Start scanning when NFC is supported
-      } else {
-        setNfcSupported(false);
-      }
-    }, []);
-  
-    const startNfcScanning = async () => {
-      try {
-        const reader = new NDEFReader();
-        await reader.scan();
-  
-        reader.addEventListener('reading', (event) => {
-          console.log('NFC tag read:', event.message.records);
-          // Process NFC tag data here
-        });
-      } catch (error) {
-        console.error('Error reading NFC:', error);
-      }
-    };
-  
+export default function Dashboard({ auth }) {
     return (
-      <div>
-        <h1>NFC Reader Web App</h1>
-        {nfcSupported === true && (
-          <p>Scanning NFC...</p>
-        )}
-        {nfcSupported === false && (
-          <p>NFC not supported in this browser.</p>
-        )}
-      </div>
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
     );
-  
 }
